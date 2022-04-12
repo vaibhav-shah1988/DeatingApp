9b9a0546f45073c6a7ae983c1d5bd358772b373b
vaibhav-shah1988 <vaibhav_shah1988@yahoo.com>
2022-04-13 01:47:52 +0530

    Intial Commit

---
+++ DatingApp.DAL/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using DatingApp.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DatingApp.DAL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
